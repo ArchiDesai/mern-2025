94a0cb3ce3fe239ba75f73bd44ca0ec9f20fd5bb
ArchiDesai <archidesai9023@gmail.com>
2025-02-21 20:55:38 +0530

    api

--- src/components/api/Api1.jsx
+++ src/components/api/Api1.jsx
@@ -1,62 +1,101 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Bounce, toast, ToastContainer } from "react-toastify";
+import Loader from "./Loader";
 
 const Api1 = () => {
-  const [message, setMessage] = useState("");
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const getData = async () => {
+    setLoading(true);
     const res = await axios.get("https://node5.onrender.com/user/user");
-    setMessage(res.data.message);
     setUsers(res.data.data);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const deleteData = async (id) => {
+    const res = await axios.delete(
+      "https://node5.onrender.com/user/user/" + id
+    );
+    toast("🦄 Data Deleted", {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
+    getData();
   };
 
   return (
     <div
       style={{
         textAlign: "center",
-        backgroundColor: "lightgray",
         height: "90vh",
         fontFamily: "Outfit,serif",
       }}
     >
-      <h1>API 1</h1>
-      <button
-        onClick={() => {
-          getData();
-        }}
-        style={{ padding: "1px 20px" }}
-      >
-        Get
-      </button>
-      <div>{message}</div>
-      <table className="table table-dark">
-        <thead>
-          <tr>
-            <td>Id</td>
-            <td>Name</td>
-            <td>Email</td>
-            <td>Age</td>
-            <td>isActive</td>
-            <td>Version</td>
-          </tr>
-        </thead>
-        {users?.map((user) => {
-          return (
-            <tbody>
-              <tr>
-                <td>{user._id}</td>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                <td>{user.age}</td>
-                <td>{user.isActive === true ? "true" : "false"}</td>
-                <td>{user.__v}</td>
-              </tr>
-            </tbody>
-          );
-        })}
-      </table>
-      ;
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
+      {loading == false && (
+        <table className="table table-dark">
+          <thead>
+            <tr>
+              <td>Id</td>
+              <td>Name</td>
+              <td>Email</td>
+              <td>Age</td>
+              <td>isActive</td>
+              <td>Version</td>
+              <td>Action</td>
+            </tr>
+          </thead>
+          {users?.map((user) => {
+            return (
+              <tbody>
+                <tr>
+                  <td>{user._id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.age}</td>
+                  <td>{user.isActive === true ? "true" : "false"}</td>
+                  <td>{user.__v}</td>
+                  <td>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => {
+                        deleteData(user._id);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            );
+          })}
+        </table>
+      )}
+      {loading == true && <Loader />}
     </div>
   );
 };
